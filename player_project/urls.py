b0cf95fc25cca3849735eeeef0202245d817ab5b
yuki-takahashi-51 <takayu151104@gmail.com>
2025-11-24 20:29:33 +0900

    Update urls.py

--- player_project/urls.py
+++ player_project/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,include
 
 #立ち上げたアプリのurlパターンを有効にするため追加する
 urlpatterns = [
-    path('player_app/', include('player_app.urls'))
+    path('', include('player_app.urls'))
 ]

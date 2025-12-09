34422fe6d1132913ae0e20a5750b4d2b5fb4c74a
yuki-takahashi-51 <takayu151104@gmail.com>
2025-12-09 23:22:06 +0900

    Update urls.py

--- player_app/urls.py
+++ player_app/urls.py
@@ -10,11 +10,12 @@ urlpatterns = [
     path("<int:uniform_number>_<str:player_name>", views.player_detail, name="player_info"),
     path("<int:uniform_number>_more_detail/", views.player_moreinfo, name="more_detail"),
     path("<int:uniform_number>/csv/", views.player_csv, name="player_csv"),
-    # path("register_user", views.register_user, name = "register_user"),
-    # path("login", views.login, name = "login"),
-    # path("register_original_batter/", views.register_original_batter, name = "register_original_batter"),
-    # path("register_original_pitcher/", views.register_original_pitcher, name = "register_original_pitcher"),
+    path("register_user", views.register_user, name = "register_user"),
+    path("login", views.login, name = "login"),
+    path("register_original_batter/", views.register_original_batter, name = "register_original_batter"),
+    path("register_original_pitcher/", views.register_original_pitcher, name = "register_original_pitcher"),
     path("original_all_player/", views.original_all_player, name = "original_all_player"),
     path("<int:uniform_number>/_more_detail", views.original_player_detail, name = "original_player_detail")
 ]
+
 
